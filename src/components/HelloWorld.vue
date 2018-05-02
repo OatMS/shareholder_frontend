e37c696d7a8f50321749697fb1e682789d60cb49
CHANNB\chanb <chenferniix@gmail.com>
2018-05-02 14:25:37 +0700

    BootstrapVue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,21 @@
-<template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+<template lang="html">
+  <div>
+    <b-dropdown id="ddown1" text="Dropdown Button" class="m-md-2">
+       <b-dropdown-item>First Action</b-dropdown-item>
+       <b-dropdown-item>Second Action</b-dropdown-item>
+       <b-dropdown-item>Third Action</b-dropdown-item>
+       <b-dropdown-divider></b-dropdown-divider>
+       <b-dropdown-item>Something else here...</b-dropdown-item>
+       <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+     </b-dropdown>
+  </div>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<script>
+export default {
+
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+</script>
+
+<style lang="css">
 </style>
