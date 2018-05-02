34298e6613da71d4b5d2921fc564516f4b414870
CHANNB\chanb <chenferniix@gmail.com>
2018-05-02 15:18:47 +0700

    Delete bootstrapVue

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+    <b-form inline>
+      <label class="sr-only" for="inlineFormInputName2">Name</label>
+      <b-input class="mb-2 mr-sm-2 mb-sm-0" id="inlineFormInputName2" placeholder="Username" />
+      <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
+      <b-input-group left="@" class="mb-2 mr-sm-2 mb-sm-0">
+        <b-input id="inlineFormInputGroupUsername2" placeholder="Password" />
+      </b-input-group>
+      <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0">Remember me</b-form-checkbox>
+      <b-button variant="primary">Login</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    { path: '/', component: HelloWorld }
+    { path: '/', component: HelloWorld },
+    { path: '/login', component: Login }
   ]
 })
