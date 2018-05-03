540f71ebc2e8b0501b014257bf2f3de959e3f8a9
Oatms <manassanan.bo@inet.co.th>
2018-05-03 14:03:19 +0700

    manage Agenda

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,17 @@ export default {
   name: 'App'
 }
 </script>
+
+<style media="screen">
+.toolbar__content{
+  background-color: green;
+}
+
+@font-face {
+    font-family: Sukhumvit;
+    src: src('@/assets/Sukhumvit.ttf');
+}
+.application {
+    font-family: Sukhumvit;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
+import ManageAgenda from '@/components/ManageAgenda'
+import QRScaner from '@/components/QRScaner'
 
 Vue.use(Router)
 
@@ -9,6 +11,8 @@ export default new Router({
   mode: 'history',
   routes: [
     { path: '/', component: HelloWorld },
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/manaegAgenda', component: ManageAgenda },
+    { path: '/qrscaner', component: QRScaner }
   ]
 })
