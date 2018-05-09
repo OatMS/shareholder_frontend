2155e8515ab4961adfa97e751d814e6b79e37e91
Oatms <manassanan.bo@inet.co.th>
2018-05-09 11:04:09 +0700

    register

---
+++ src/components/contents/RegisterShareholder.vue
@@ -0,0 +1,76 @@
+<template lang="html">
+  <div  class="regiterationLayout text-xs-center">
+    <h1 style="font-weight: 900; font-size:2.5em">ลงทะเบียนเข้างาน</h1>
+    <v-layout row wrap class="mt-4">
+      <v-flex xs12 sm2>
+        <h1 >ค้นหา</h1>
+      </v-flex>
+      <v-flex xs12 sm8>
+        <!-- <v-text-field v-model="searchText" v-on:change="searchShareholder" solo label="ชื่อ / นามสกุล / บริษัท / เลขผู้ถือหุ้น"></v-text-field> -->
+        <v-select
+                    ref="shareholder"
+                    :items="shareholder"
+                    v-model="searchText"
+                    autocomplete
+                    label=""
+                    placeholder="ชื่อ / นามสกุล / บริษัท / เลขผู้ถือหุ้น"
+                    required
+                  ></v-select>
+
+      </v-flex>
+
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      searchText: '',
+      shareholder: []
+    }
+  },
+  methods: {
+    // searchShareholder () {
+    //   this.axios.post('203.154.58.87:5000' + '/searchShareholder', { 'str': this.searchText }).then((response) => {
+    //     var result = JSON.parse(response.data)
+    //     if (response.status === 200) {
+    //       if (result.length > 0) {
+    //         this.profileImg = result[0]['profile_image']
+    //       }
+    //     }
+    //   })
+    // }
+  },
+  mounted () {
+    this.axios.get('http://203.154.58.87:5000' + '/getAllShareholder/2/2018').then((response) => {
+      // var result = JSON.parse(response.data)
+      var result = response.data
+      if (response.status === 200) {
+        this.shareholder = result
+        console.log(result)
+      }
+    })
+    // this.shareholder = [{ 'text': 'มนัสนันท์ บุญนวสิน อินเตอร์เน็ตประเทศไทย จำกัดมหาชน(INET)' }, { 'text': 'สมชาย นามสมมติ ลูกไก่น้อย' }]
+  }
+
+}
+</script>
+
+<style lang="css">
+
+.regiterationLayout{
+  /* border: solid;  */
+  width : 70%;
+  height: 70%;
+  margin: 10% auto;
+  /* text-align: center; */
+}
+
+.searchShareholder{
+  margin-top: 25px;
+}
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,55 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login/Login'
 import ManageAgenda from '@/components/ManageAgenda'
+import RegisterShareholder from '@/components/contents/RegisterShareholder'
+import AgendaList from '@/components/contents/AgendaList'
 import QRScaner from '@/components/QRScaner'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     { path: '/login', component: Login },
     { path: '/manageAgenda', component: ManageAgenda },
-    { path: '/qrscaner', component: QRScaner }
+    { path: '/qrscaner', component: QRScaner },
+    { path: '/',
+      component: ManageAgenda,
+      // meta: { requiresAuth: true },
+      children: [
+        { path: '/', component: RegisterShareholder },
+        { path: '/agendaList', component: AgendaList }
+        // { path: '/register/:id', component: register },
+        // { path: '/all', component: all },
+        // { path: '/newaccount', component: newaccount },
+        // { path: '/resendMail', component: resendMail }
+      ]
+    }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((x) => x.meta.requiresAuth)) {
+    if (!window.$cookies.isKey('username_inet_register')) {
+      next({ path: '/' })
+    } else {
+      next()
+    }
+    return
+  }
+  next()
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((x) => x.meta.isAuth)) {
+    if (window.$cookies.isKey('username_inet_register')) {
+      next({ path: '/' })
+    } else {
+      next()
+    }
+    return
+  }
+  next()
+})
+
+export default router
