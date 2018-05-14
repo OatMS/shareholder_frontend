6a8dddec3d6aab02bba621ea1211472317e1888c
Oatms <manassanan.bo@inet.co.th>
2018-05-14 10:10:40 +0700

    register complete

--- src/App.vue
+++ src/App.vue
@@ -15,37 +15,35 @@ export default {
   background-color: green;
 }
 
-@font-face {
-    font-family: Sukhumvit;
-    src: url('/static/font/Sukhumvit.ttf') format('truetype');
-}
-
-
-@font-face {
+/* @font-face {
   font-family: Sukhumvit;
-  src: url("/static/font/Sukhumvit.ttf") format('truetype');
+  src: url("/static/font/Sukhumvit-Bold.ttf") format('truetype');
   font-weight: bold;
-}
+} */
 
-@font-face {
+/* @font-face {
   font-family: Sukhumvit;
-  src: url("/static/font/Sukhumvit.ttf") format('truetype');
+  src: url("/static/font/Sukhumvit-Thin.ttf") format('truetype');
   font-weight: thin;
 }
 
 @font-face {
   font-family: Sukhumvit;
-  src: url("/static/font/Sukhumvit.ttf") format('truetype');
+  src: url("/static/font/Sukhumvit-Medium.ttf") format('truetype');
   font-weight: medium;
 }
 
 @font-face {
   font-family: Sukhumvit;
-  src: url("/static/font/Sukhumvit.ttf") format('truetype');
+  src: url("/static/font/Sukhumvit-Light.ttf") format('truetype');
   font-weight: lighter;
+} */
+@font-face {
+    font-family: Sukhumvit;
+    src: url('/static/font/Sukhumvit.ttf') format('truetype');
 }
 div {
   font-family: Sukhumvit;
-  font-weight: 600;
+  /* font-weight: 600; */
 }
 </style>
--- src/components/contents/RegisterShareholder.vue
+++ src/components/contents/RegisterShareholder.vue
@@ -3,7 +3,7 @@
     <h1 style="font-weight: 900; font-size:2.5em">ลงทะเบียนเข้างาน</h1>
     <v-layout row wrap class="mt-4">
       <v-flex xs12 sm2>
-        <h1 >ค้นหา</h1>
+        <h2 >ค้นหา</h2>
       </v-flex>
       <v-flex xs12 sm8>
         <!-- <v-text-field v-model="searchText" v-on:change="searchShareholder" solo label="ชื่อ / นามสกุล / บริษัท / เลขผู้ถือหุ้น"></v-text-field> -->
@@ -21,113 +21,133 @@
       </v-flex>
     </v-layout>
     <div class="formInfo" v-if="showInfo" v-show="showInfo">
-      <v-layout row wrap class="mt-4" v-show="selectedItem.info.title != 'บริษัท' ">
-        <v-flex xs12 sm2>
-          <h2>ชื่อ-นามสกุล :</h2>
-        </v-flex>
-        <v-flex xs12 sm6>
-          <h2>{{selectedItem.info.title}} {{selectedItem.info.firstname}} {{selectedItem.info.lastname}}</h2>
-        </v-flex>
-      </v-layout>
-      <v-layout row wrap class="mt-4" v-show="selectedItem.info.title == 'บริษัท' ">
-        <v-flex xs12 sm2>
-          <h2>ชื่อบริษัท :</h2>
-        </v-flex>
-        <v-flex xs12 sm8>
-          <h2>{{selectedItem.info.companyname}} </h2>
-        </v-flex>
+      <div class="infoDiv">
+        <v-layout row wrap v-show="selectedItem.info.title != 'บริษัท' ">
+          <v-flex xs12 sm2>
+            <h3>ชื่อ-นามสกุล :</h3>
+          </v-flex>
+          <v-flex xs12 sm7>
+            <h3>{{selectedItem.info.title}} {{selectedItem.info.firstname}} {{selectedItem.info.lastname}}</h3>
+          </v-flex>
+          <v-flex xs12 sm3 class="text-xs-right">
+            <v-btn v-show="selectedItem.info.register != 0" style="width:100% height:60px;font-weight: bold;" color="primary darken-2" dark>พิมพ์บัตรลงคะแนน</v-btn>
+          </v-flex>
+        </v-layout>
+        <v-layout row wrap class="mt-4" v-show="selectedItem.info.title == 'บริษัท' ">
+          <v-flex xs12 sm2>
+            <h3>ชื่อบริษัท :</h3>
+          </v-flex>
+          <v-flex xs12 sm7>
+            <h3>{{selectedItem.info.companyname}} </h3>
+          </v-flex>
+          <v-flex xs12 sm3 class="text-xs-right">
+            <v-btn style="width:100% height:60px;font-weight: bold;" color="primary darken-2" dark >พิมพ์บัตรลงคะแนน</v-btn>
+          </v-flex>
 
-      </v-layout>
-      <v-layout row wrap class="mt-4">
-        <v-flex xs12 sm2>
-          <h2>จำนวนหุ้น</h2>
-        </v-flex>
-        <v-flex xs12 sm3>
-          <h2>{{selectedItem.info.q_share.toLocaleString()}}</h2>
-        </v-flex>
-        <v-flex xs12 sm1>
-          <h2>หุ้น</h2>
-        </v-flex>
-      </v-layout>
-      <v-layout row wrap class="mt-4">
-        <v-flex xs12 sm2>
-          <h2>สถานะ :</h2>
-        </v-flex>
-        <v-flex xs12 sm4>
-          <h2 v-show="selectedItem.info.register != 0">ยังไม่ได้ลงทะเบียน</h2>
-          <h2 v-show="selectedItem.info.register == 0">ลงทะเบียนแล้ว</h2>
-        </v-flex>
-      </v-layout>
-      <v-layout row wrap class="mt-4">
-        <v-flex xs12 sm2 >
-          <v-btn  v-show="selectedItem.info.register != 0" style="width:100% height:60px" @click.stop="dialog2 = true" large color="success">ผู้มอบฉันทะ</v-btn>
-          <v-btn  v-show="selectedItem.info.register == 0" style="width:100% height:60px" @click.stop="dialog2 = true" large disabled >ผู้มอบฉันทะ</v-btn>
-        </v-flex>
-        <v-flex xs12 sm6>
-          <!-- <h2>สถานะ :</h2> -->
-        </v-flex >
-        <v-flex xs12 sm2 style="text-align:right;" >
-          <v-btn v-show="selectedItem.info.register != 0" style="width:100% height:60px" large color="success">ลงทะเบียน</v-btn>
-          <v-btn v-show="selectedItem.info.register == 0" style="width:100% height:60px" large  disabled>ลงทะเบียน</v-btn>
-        </v-flex>
+        </v-layout>
+        <v-layout row wrap class="mt-4">
+          <v-flex xs12 sm2>
+            <h3>จำนวนหุ้น</h3>
+          </v-flex>
+          <v-flex xs12 sm2>
+            <h3>{{selectedItem.info.q_share.toLocaleString()}}</h3>
+          </v-flex>
+          <v-flex xs12 sm1>
+            <h3>หุ้น</h3>
+          </v-flex>
+        </v-layout>
+        <v-layout row wrap class="mt-4">
+          <v-flex xs12 sm2>
+            <h3>สถานะ :</h3>
+          </v-flex>
+          <v-flex xs12 sm4>
+            <h3 v-show="selectedItem.info.register == 0">ยังไม่ได้ลงทะเบียน</h3>
+            <h3 v-show="selectedItem.info.register != 0">ลงทะเบียนแล้ว</h3>
+          </v-flex>
+        </v-layout>
+        <v-layout row wrap class="mt-4">
+          <v-flex xs12 sm12  class="text-xs-right">
+            <v-btn  v-show="selectedItem.info.register == 0" style="width:100% height:60px;font-weight: bold;" @click.stop="proxyDialog = true" large color="amber darken-2" dark depressed>ผู้มอบฉันทะ</v-btn>
+            <v-btn  v-show="selectedItem.info.register != 0" style="width:100% height:60px;font-weight: bold;" @click.stop="proxyDialog = true" large disabled >ผู้มอบฉันทะ</v-btn>
+          <!-- </v-flex>
+          <v-flex xs12 sm3 class="text-xs-right"> -->
+            <v-btn v-show="selectedItem.info.register == 0" @click.native.stop="registerDialog = true" style="width:100% height:60px;background-color:#61C356;" large dark depressed>ลงทะเบียน</v-btn>
+            <v-btn v-show="selectedItem.info.register != 0" style="width:100% height:60px" large  disabled>ลงทะเบียน</v-btn>
+          </v-flex>
 
-      </v-layout>
+        </v-layout>
 
-      <v-dialog v-model="dialog2" width="60%">
-        <v-card>
-          <v-card-title>
-            <h2>ลงทะเบียนแบบมอบฉันทะ</h2>
-          </v-card-title>
-          <v-card-text>
-            <v-layout row wrap class="mt-4">
-              <v-flex xs12 sm2 class="pd5" >
-                <v-select
-                ref="shareholder"
-                :items="title"
-                solo
-                v-model="proxyTitle"
-                autocomplete
-                label=""
-                placeholder="title"
-                required
-                ></v-select>
-              </v-flex>
-              <v-flex xs12 sm5 class="pd5">
-                <v-text-field v-model="proxyName"  solo label="ชื่อ"></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm5 class="pd5">
-                <v-text-field v-model="proxyLastname"  solo label="นามสกุล"></v-text-field>
-              </v-flex >
-              <!-- <v-flex xs12 sm2 style="text-align:right;" >
-                <v-btn style="width:100% height:60px" large color="success">ลงทะเบียน</v-btn>
-              </v-flex> -->
+        <v-dialog v-model="proxyDialog" width="60%">
+          <v-form ref="form">
 
-            </v-layout>
-            <v-layout row wrap class="mt-4">
-              <v-flex xs12 sm2 >
-                <!-- <v-btn style="width:100% height:60px" @click.stop="dialog2 = true" large color="success">ผู้มอบฉันทะ</v-btn> -->
-              </v-flex>
-              <v-flex xs12 sm2>
-                <!-- <h2>สถานะ :</h2> -->
-                <v-btn style="width:100% height:60px" @click.stop="dialog2=false" large >ย้อนกลับ</v-btn>
+          <v-card>
+            <v-card-title>
+              <h2>ลงทะเบียนแบบมอบฉันทะ</h2>
+            </v-card-title>
+            <v-card-text >
+              <v-layout row wrap  class="mt-4">
+                <v-flex xs12 sm2 class="pd5"  >
+                  <v-select
+                  ref="proxyTitle"
+                  :items="title"
+                  v-model="proxyTitle"
+                  autocomplete
+                  label=""
+                  placeholder="คำนำหน้า"
+                  :rules="[() => !!proxyTitle || 'This field is required']"
+                  required
+                  ></v-select>
+                </v-flex>
+                <v-flex xs12 sm5 class="pd5">
+                  <v-text-field v-model="proxyName"  label="ชื่อ"
+                  ref="proxyName"
+                  :rules="[() => !!proxyName || 'This field is required']" required>
 
-              </v-flex >
-              <v-flex xs12 sm4>
-                <!-- <h2>สถานะ :</h2> -->
-              </v-flex >
-              <v-flex xs12 sm2 style="text-align:right;" >
-                <v-btn style="width:100% height:60px" large color="success">ลงทะเบียน</v-btn>
-              </v-flex>
+                  </v-text-field>
+                </v-flex>
+                <v-flex xs12 sm5 class="pd5">
+                  <v-text-field v-model="proxyLastname" label="นามสกุล"
+                  ref="proxyLastname"
+                  :rules="[() => !!proxyLastname || 'This field is required']"
+                  required ></v-text-field>
+                </v-flex >
 
-            </v-layout>
-          </v-card-text>
-          <!-- <v-card-actions>
-            <v-btn color="primary" flat >Close</v-btn>
-          </v-card-actions> -->
-        </v-card>
-      </v-dialog>
+              </v-layout>
+              <v-layout row wrap class="mt-4">
+                <v-flex xs12 sm2 >
+                </v-flex>
+                <v-flex xs12 sm2>
+                  <v-btn style="width:100% height:60px" @click="clearProxyDialog" @click.stop="proxyDialog=false" large >ย้อนกลับ</v-btn>
 
+                </v-flex >
+                <v-flex xs12 sm4>
+                </v-flex >
+                <v-flex xs12 sm2 style="text-align:right;" >
+                  <v-btn style="width:100% height:60px" @click='register("proxy")' large color="success">ลงทะเบียน</v-btn>
+                </v-flex>
 
+              </v-layout>
+            </v-card-text>
+          </v-card>
+
+        </v-form>
+        </v-dialog>
+
+        <v-layout row justify-center>
+          <!-- <v-btn color="primary" dark >Open Dialog</v-btn> -->
+          <v-dialog v-model="registerDialog"  max-width="400">
+            <v-card >
+              <v-card-title ></v-card-title>
+              <v-card-text style="text-align:center;"><h2>ยืนยันการละทะเบียนผู้ถือหุ้น</h2></v-card-text>
+              <v-card-actions>
+                <v-btn color="green darken-1" flat="flat" @click.native="registerDialog = false">ย้อนกลับ</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="green darken-1" flat="flat" @click="register('simple')" @click.native="registerDialog = false">ยืนยัน</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-layout>
+      </div>
 
     </div>
   </div>
@@ -143,25 +163,65 @@ export default {
     return {
       selectedItem: {},
       shareholder: [],
-      dialog2: false,
-      title: [ 'นาย', 'นาง', 'นางสาว' ],
+      proxyDialog: false,
+      registerDialog: false,
+      title: [ 'นาย', 'นาง', 'น.ส.' ],
       proxyTitle: '',
       proxyName: '',
       proxyLastname: '',
-      showInfo: false
+      showInfo: false,
+      errorMessages: [],
+      checkProxyForm: false
     }
   },
   methods: {
-    // searchShareholder () {
-    //   this.axios.post('203.154.58.87:5000' + '/searchShareholder', { 'str': this.searchText }).then((response) => {
-    //     var result = JSON.parse(response.data)
-    //     if (response.status === 200) {
-    //       if (result.length > 0) {
-    //         this.profileImg = result[0]['profile_image']
-    //       }
-    //     }
-    //   })
-    // }
+    register (registerBy) {
+      var data = {}
+      if (registerBy === 'proxy') {
+        if (this.proxyTitle === '' || this.proxyName === '' || this.proxyLastname === '') {
+          this.checkProxyForm = true
+          this.$swal('กรุณากรอกข้อมูลให้ครบค่ะ', '', 'error')
+          this.Loading = false
+          return
+        } else {
+          data = {
+            'registertype': registerBy,
+            'proxytitle': this.proxyTitle,
+            'proxyname': this.proxyName,
+            'proxylastname': this.proxyLastname,
+            'memberid': this.selectedItem.info.memberid,
+            'registerby': 'manassanan.bo' }
+        }
+      } else {
+        data = {
+          'registertype': registerBy,
+          'memberid': this.selectedItem.info.memberid,
+          'registerby': 'manassanan.bo' }
+      }
+
+      this.axios.post('http://203.154.58.87:5000' + '/register/2/2018', data).then((response) => {
+        // var result = JSON.parse(response.data)
+        var result = response.data
+        if (response.status === 200) {
+          if (result.status === 'success') {
+            this.clearProxyDialog()
+            console.log(result)
+            this.selectedItem.info.register = result.info
+            this.shareholder[result.info.id - 1].info = result.info
+            // --- register success -----
+          }
+        }
+      })
+    },
+    clearProxyDialog () {
+      // this.proxyTitle = ''
+      // this.proxyName = ''
+      // this.proxyLastname = ''
+      // Object.keys(this.proxyTitle)[0].reset()
+      // Object.keys(this.proxyName)[0].reset()
+      // Object.keys(this.proxyLastname)[0].reset()
+      this.$refs.form.reset()
+    }
   },
   mounted () {
     this.axios.get('http://203.154.58.87:5000' + '/getAllShareholder/2/2018').then((response) => {
@@ -173,6 +233,15 @@ export default {
       }
     })
     // this.shareholder = [{ 'text': 'มนัสนันท์ บุญนวสิน อินเตอร์เน็ตประเทศไทย จำกัดมหาชน(INET)' }, { 'text': 'สมชาย นามสมมติ ลูกไก่น้อย' }]
+  },
+  computed: {
+    // form () {
+    //   return {
+    //     title: this.proxyTitle,
+    //     name: this.proxyName,
+    //     lastname: this.proxyLastname
+    //   }
+    // }
   },
   watch: {
     selectedItem () {
@@ -212,7 +281,11 @@ export default {
   margin-top:10%;
   text-align: left;
 }
-
+.infoDiv{
+  border: 2px solid #ccc;
+  border-radius: 1em;
+  padding: 2em;
+}
 .pd5{
   padding: 5px;
 }
