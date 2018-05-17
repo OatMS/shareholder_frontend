5a931eb678abd79379ad839ae3bda1e3c734d26b
CHANNB\chanb <chenferniix@gmail.com>
2018-05-17 17:44:13 +0700

    update bg

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -2,28 +2,32 @@
 
   <div>
     <v-app light>
-      <v-toolbar class="white">
-        <v-toolbar-title class="white--text" v-text="header"></v-toolbar-title>
-      </v-toolbar>
+      <v-footer height="80" color="green" class="pa-3">
+        <v-flex xs12 py-3 text-xs-center white--text>
+          <h1>Internet Thailand Public Company Limited {{ new Date().getFullYear() }}</h1>
+        </v-flex>
+      </v-footer>
       <v-content>
         <section>
-          <v-parallax src="/static/img/icons/103.jpg" height="600">
+          <v-parallax src="/static/img/icons/103.jpg" height="610">
             <v-layout
               column
               align-center
               justify-center
               class="white--text"
               >
-              <img src="/static/img/icons/102.png" height="200" class="mb-5">
-              <h1 class="white--text mb-2 display-1 text-xs-center">Annual General Meeting of Shareholders {{ new Date().getFullYear() }}</h1>
-              <div class="subheading mb-3 text-xs-center">การประชุมสามัญผู้ถือหุ้นประจำปี {{ new Date().getFullYear() }}</div>
+              <img src="/static/img/icons/logo.gif" height="100" class="mb-5">
+              <h1 class="white--text mb-4 display-1 text-xs-center">Annual General Meeting of Shareholders {{ new Date().getFullYear() }}</h1>
+              <div class="subheading text-xs-center">การประชุมสามัญผู้ถือหุ้นประจำปี {{ new Date().getFullYear() }}</div>
             </v-layout>
           </v-parallax>
         </section>
       </v-content>
-      <v-toolbar class="white">
-        <v-toolbar-title class="white--text" v-text="title"></v-toolbar-title>
-      </v-toolbar>
+      <v-footer height="82" color="green" class="pa-3">
+        <v-flex xs12 py-3 text-xs-center white--text>
+          <v-pagination :length="4" v-model="page" circle></v-pagination>
+        </v-flex>
+      </v-footer>
     </v-app>
   </div>
 </template>
@@ -33,7 +37,8 @@ export default {
   data () {
     return {
       title: 'Annual General Meeting of Shareholders',
-      header: 'Internet Thailand Public Company Limited'
+      header: 'Internet Thailand Public Company Limited',
+      page: 1
     }
   }
 }
